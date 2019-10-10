c144f59ed1def5bd05a32bf380e4c02a29a3c2b6
augusto642 <44950500+augusto642@users.noreply.github.com>
2019-10-10 20:10:20 -0300

    Add files via upload

---
+++ classes/Sistema.php
@@ -0,0 +1,21 @@
+<?php
+
+class Sistema
+{
+
+
+    public static function carregarPagina()
+    {
+        if (isset( $_GET['pg'] )) {
+            include $_GET['pg'] . ".php"; // onde 'pg' é a variavel passada pela URL (GET)
+        } else {
+            include 'home.php'; //primeiro acesso, padrao 'home.php'
+        }
+    }
+    public static function logado(){
+        return isset($_SESSION['login']) ? 1: 0;
+    }
+    public static function loggout(){
+        session_destroy();
+    }
+}
